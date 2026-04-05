ccaa41e99a8fe860e432a321a0e78f4b4f07c9a2
Sundaramoorthy <sundar3301@gmail.com>
2026-04-06 01:26:53 +0530

    Improved retention query

---
+++ metrics/retention_v2.sql
@@ -0,0 +1,19 @@
+-- Retention Metrics (Practice)
+WITH customer_metrics AS (
+    SELECT
+        DATE_TRUNC(order_date, MONTH) AS month,
+        customer_id,
+        is_retained,
+    FROM orders
+)
+
+SELECT
+    month,
+    COUNT(DISTINCT customer_id) AS total_customers,
+    COUNT(DISTINCT CASE WHEN is_retained THEN customer_id END) AS retained_customers,
+    SAFE_DIVIDE(COUNT(DISTINCT CASE WHEN is_retained THEN customer_id END), 
+                    COUNT(DISTINCT customer_id))
+                     AS retention_rate
+FROM customer_metrics
+GROUP BY month
+ORDER BY month;
